311f160c161b8d228ef074be9a09f9229595f0c6
Johnrey <johnrey.bacal@yahoo.com>
2024-07-18 20:51:49 +0800

    add base definition

---
+++ models/base.ts
@@ -0,0 +1,52 @@
+import { DataTypes, Sequelize } from "sequelize";
+import BaseDefinition from "../definitions/base";
+import { utils } from "../definitions/util";
+
+export const define = (
+    sequelize: Sequelize,
+    definition: BaseDefinition,
+    modelName: string,
+    defaultTableName: string,
+    columns = {}
+) => {
+    Object.assign(columns, {
+        [definition.name]: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    });
+
+    utils.addColumnIfDefined<BaseDefinition>(
+        columns,
+        definition,
+        "code",
+        DataTypes.STRING,
+        false
+    );
+    utils.addColumnIfDefined<BaseDefinition>(
+        columns,
+        definition,
+        "oldCode",
+        DataTypes.STRING,
+        true
+    );
+    utils.addColumnIfDefined<BaseDefinition>(
+        columns,
+        definition,
+        "population",
+        DataTypes.INTEGER,
+        true
+    );
+
+    const model = sequelize.define(modelName, columns, {
+        tableName: definition.tableName ?? defaultTableName,
+        createdAt: definition.createdAt ?? false,
+        updatedAt: definition.updatedAt ?? false,
+    });
+
+    if (!definition.id) {
+        model.removeAttribute("id");
+    }
+
+    return model;
+};
