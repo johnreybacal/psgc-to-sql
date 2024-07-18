3e4e6f666546addcfa82fd5733712480dd8ecb93
Johnrey <johnrey.bacal@yahoo.com>
2024-07-18 20:52:03 +0800

    update define functions

--- models/province.ts
+++ models/province.ts
@@ -1,40 +1,19 @@
 import { DataTypes, Sequelize } from "sequelize";
 import ProvinceDefinition from "../definitions/province";
 import { utils } from "../definitions/util";
+import { define } from "./base";
 
-const define = (sequelize: Sequelize, definition: ProvinceDefinition) => {
-    const columns = {
-        [definition.name]: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-    };
-
-    utils.checkProperty<ProvinceDefinition>(
-        columns,
-        definition,
-        "code",
-        DataTypes.STRING,
-        false
-    );
-    utils.checkProperty<ProvinceDefinition>(
+export const defineProvince = (
+    sequelize: Sequelize,
+    definition: ProvinceDefinition
+) => {
+    const columns = {};
+    utils.addColumnIfDefined<ProvinceDefinition>(
         columns,
         definition,
         "incomeClassification",
         DataTypes.STRING
     );
 
-    const Province = sequelize.define("Province", columns, {
-        tableName: definition.tableName ?? "provinces",
-        createdAt: definition.createdAt ?? false,
-        updatedAt: definition.updatedAt ?? false,
-    });
-
-    if (!definition.id) {
-        Province.removeAttribute("id");
-    }
-
-    return Province;
+    return define(sequelize, definition, "Province", "provinces", columns);
 };
-
-export default define;
--- models/region.ts
+++ models/region.ts
@@ -1,28 +1,10 @@
-import { DataTypes, Sequelize } from "sequelize";
+import { Sequelize } from "sequelize";
 import RegionDefinition from "../definitions/region";
-import { utils } from "../definitions/util";
+import { define } from "./base";
 
-const define = (sequelize: Sequelize, definition: RegionDefinition) => {
-    const columns = {
-        [definition.name]: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-    };
-
-    utils.checkProperty(columns, definition, "code", DataTypes.STRING, false);
-
-    const Region = sequelize.define("Region", columns, {
-        tableName: definition.tableName ?? "regions",
-        createdAt: definition.createdAt ?? false,
-        updatedAt: definition.updatedAt ?? false,
-    });
-
-    if (!definition.id) {
-        Region.removeAttribute("id");
-    }
-
-    return Region;
+export const defineRegion = (
+    sequelize: Sequelize,
+    definition: RegionDefinition
+) => {
+    return define(sequelize, definition, "Region", "regions");
 };
-
-export default define;
